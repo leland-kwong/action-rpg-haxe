8cd26dcabddddf4c9502875c0fe7ed68605be4ae
leland-kwong <leland.kwong@gmail.com>
2020-07-21 20:27:31 -0400

    Update loot definitions

--- src/Loot.hx
+++ src/Loot.hx
@@ -1,24 +1,35 @@
-typedef LootDefKey = String;
+typedef LootDefType = String;
+
+typedef LootDefCat = String;
 
 typedef LootDef = {
   name: String,
+  type: LootDefType,
+  category: LootDefCat,
   minDamage: Int,
   maxDamage: Int,
   spriteKey: String
 };
 
+// loot that was generated via rng
 typedef LootInstance = {
-  type: LootDefKey
+  type: LootDefType
 };
 
 class Loot {
-  static final defs: Map<LootDefKey, LootDef> = [
-    'spiderBots' => {
-      name: 'Spider Bots',
-      minDamage: 1,
-      maxDamage: 1,
-      spriteKey: 'ui/loot__ability_spider_bot'
-    }
+  static final lootDefinitions: Array<LootDef> = [
+  {
+    name: 'Spider Bots',
+    type: 'spiderBots',
+    category: 'ability',
+    minDamage: 1,
+    maxDamage: 1,
+    spriteKey: 'ui/loot__ability_spider_bot'
+  }
+  ];
+
+  static final defs: Map<LootDefType, LootDef> = [
+    for (def in lootDefinitions) def.type => def
   ];
 
   public static function getDef(type): LootDef {
